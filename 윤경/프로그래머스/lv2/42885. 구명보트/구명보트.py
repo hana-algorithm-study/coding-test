caa018da503a58c64a97d6671d911d6a3a38530a
ykpark <63557248+ykpark3@users.noreply.github.com>
2023-06-01 10:35:57 +0900

    [level 2] Title: 구명보트, Time: 8.14 ms, Memory: 10.5 MB -BaekjoonHub

--- 윤경/프로그래머스/lv2/42885. 구명보트/구명보트.py
+++ 윤경/프로그래머스/lv2/42885. 구명보트/구명보트.py
@@ -1,27 +1,3 @@
 def solution(people, limit):
     answer = 0
-    people.sort()
-    two = []
-    one = []
-    all = len(people)
-    cnt = 0
-    
-    s = 0
-    e = len(people)-1
-    
-    while s <= e:
-        if people[s] + people[e] <= limit and s != e:
-            two.append(people[s])
-            two.append(people[e])
-            s += 1
-            e -= 1
-            cnt += 1
-        else:
-            one.append(people[e])
-            e -= 1
-            cnt += 1
-    
-        
-    answer = cnt   
-    
     return answer
