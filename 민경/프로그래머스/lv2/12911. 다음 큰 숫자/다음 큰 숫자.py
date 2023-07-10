f6303c127ae40c7c38320eeadb703e26282605b9
MINKYUNG <132539247+LaNiMk@users.noreply.github.com>
2023-07-10 13:23:12 +0900

    Revert "[민경] 7주차: 12911. 다음 큰 숫자 (#63)"
    This reverts commit aac7d1a390b8f86b6340d64a6d02fa38b0b898bd.

--- 민경/프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.py
+++ 민경/프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.py
@@ -1,7 +1,3 @@
 def solution(n):
-    bin_n = list(bin(n)[2:]).count('1')
-    answer = n + 1
-    while True :
-        one = list(bin(answer)[2:]).count('1')
-        if one == bin_n : return answer
-        else : answer += 1
+    answer = 0
+    return answer
