c0f43199f830135e30b7150ce71f42d97b369b61
MINKYUNG <132539247+LaNiMk@users.noreply.github.com>
2023-07-13 13:15:13 +0900

    [민경] 6주차: 42747. H-Index (#66)

--- 민경/프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.py
+++ 민경/프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.py
@@ -1,3 +1,7 @@
 def solution(n):
-    answer = 0
-    return answer
+    bin_n = list(bin(n)[2:]).count('1')
+    answer = n + 1
+    while True :
+        one = list(bin(answer)[2:]).count('1')
+        if one == bin_n : return answer
+        else : answer += 1
